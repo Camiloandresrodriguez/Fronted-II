d392905b6816dabefad7e2626818a983077ac23b
Camiloandresrodriguez <camirodoq88@gmail.com>
2026-01-26 11:17:54 -0500

    Se realiza proyecto react de prueba

---
+++ ejercicios/15122025/ejercicioEnClase/main.js
@@ -0,0 +1,10 @@
+function sumar(params)
+{
+    let a = parseInt(prompt("Digite el primer numero: "))
+    let b = parseInt(prompt("Digite el segundo numero: "))
+    console.log(typeof(a));
+    console.log(typeof(b));
+    console.log(a + b);
+    
+}
+sumar()
